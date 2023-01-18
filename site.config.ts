50fd08bd33d5700e6f369da95006531536eace0f
Thomas O'Neill <tom@opensystemslab.io>
2023-01-17 17:47:39 -0800

    update site config

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,8 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '33244ffc5ea649b6bcd217b13c361250',
+
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +11,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  domain: 'crosswires.dev',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
